6c9ddd58e4b259a8fd5c35ced3b0d6b74311cf68
renandcisuporte <renan@dcisuporte.com.br>
2024-03-23 11:48:29 -0300

    fix: ajusta arquivos

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiResponse({
+    status: 200,
+    description: 'The found record',
+    type: null,
+  })
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return { data: this.appService.getHello() };
   }
 }
