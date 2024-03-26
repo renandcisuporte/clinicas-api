bdc89f1e5295e17011da4fecacdd50e86887175d
renandcisuporte <renan@dcisuporte.com.br>
2024-03-26 12:26:01 -0300

    feat: novo method

--- src/users/repositories/user.repository.ts
+++ src/users/repositories/user.repository.ts
@@ -3,4 +3,5 @@ import { UserEntity } from '../entities/user.entity';
 
 export abstract class UserRepository {
   abstract createUser(input: CreateUserDTO): Promise<UserEntity>;
+  abstract isMatchUser(email: string): Promise<boolean>;
 }
