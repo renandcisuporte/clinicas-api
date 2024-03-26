3672b94386191ac4788d0f4065f4e6140baa248f
renandcisuporte <renan@dcisuporte.com.br>
2024-03-26 15:45:06 -0300

    feat: novo caso de uso

--- src/users/use-cases/create-user.use-case.ts
+++ src/users/use-cases/create-user.use-case.ts
@@ -1,19 +1,26 @@
 import { ForbiddenException, Inject, Injectable } from '@nestjs/common';
 import { CreateUserDTO } from '../dtos/create-user.dto';
 import { UserRepository } from '../repositories/user.repository';
+import { Bcrypt } from 'src/utils/shared/bcrypt.shared';
 
 @Injectable()
 export class CreateUserUseCase {
   constructor(
     @Inject('UserRepository')
     private readonly userRepo: UserRepository,
+
+    @Inject('Bcrypt')
+    private readonly bcrypt: Bcrypt,
   ) {}
 
   async execute(input: CreateUserDTO) {
-    const isMacth = this.userRepo.isMatchUser(input.email);
+    const isMacth = await this.userRepo.alreadyExists(input.email);
     if (isMacth) {
       throw new ForbiddenException({ message: 'user already exists' });
     }
-    return await this.userRepo.createUser(input);
+
+    const password = await this.bcrypt.hash(input.password);
+
+    return await this.userRepo.createUser({ ...input, password });
   }
 }
